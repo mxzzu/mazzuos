3456cbd325a5babeae18204321d4b3f5f06b6028
Riccardo Mazzurana <riccardo.mazzurana@gmail.com>
2026-05-30 16:25:16 +0200

    First Personalization

--- Containerfile
+++ Containerfile
@@ -4,30 +4,19 @@ COPY build_files /
 
 # Base Image
 FROM registry.gitlab.com/origami-linux/images/origami:latest
+RUN sed -i 's/^NAME=.*/NAME="MazzuOS"/' /etc/os-release
+RUN sed -i 's/^PRETTY_NAME=.*/PRETTY_NAME="MazzuOS Linux"/' /etc/os-release
+RUN sed -i 's/^ID=.*/ID=mazzuos/' /etc/os-release
 
-## Other possible base images include:
-# FROM ghcr.io/ublue-os/bazzite:latest
-# FROM ghcr.io/ublue-os/bluefin-nvidia:stable
-# 
-# ... and so on, here are more base images
-# Universal Blue Images: https://github.com/orgs/ublue-os/packages
-# Fedora base image: quay.io/fedora/fedora-bootc:41
-# CentOS base images: quay.io/centos-bootc/centos-bootc:stream10
-
-### [IM]MUTABLE /opt
-## Some bootable images, like Fedora, have /opt symlinked to /var/opt, in order to
-## make it mutable/writable for users. However, some packages write files to this directory,
-## thus its contents might be wiped out when bootc deploys an image, making it troublesome for
-## some packages. Eg, google-chrome, docker-desktop.
-##
-## Uncomment the following line if one desires to make /opt immutable and be able to be used
-## by the package manager.
-
-# RUN rm /opt && mkdir /opt
-
-### MODIFICATIONS
-## make modifications desired in your image and install packages by modifying the build.sh script
-## the following RUN directive does all the things required to run "build.sh" as recommended.
+# Copy Homebrew files from the brew image
+# And enable
+COPY --from=ghcr.io/ublue-os/brew:latest /system_files /
+RUN --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    /usr/bin/systemctl preset brew-setup.service && \
+    /usr/bin/systemctl preset brew-update.timer && \
+    /usr/bin/systemctl preset brew-upgrade.timer
 
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,76 @@
 
 set -ouex pipefail
 
-### Install packages
+## DNF5 Speedup
+sed -i '/^\[main\]/a max_parallel_downloads=10' /etc/dnf/dnf.conf
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+## 2. Remove COSMIC Shell and Waybar
+dnf -y remove cosmic-comp cosmic-initial-setup cosmic-settings cosmic-settings-daemon cosmic-store waybar
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+## 3. Install GNOME DE
+dnf -y group install "GNOME" --with-optional
+systemctl enable gdm.service
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# System apps
+dnf install -y libvirt virt-manager qemu-kvm bitwarden-cli
 
-#### Example for enabling a System Unit File
+# User apps
+dnf install -y kitty zsh
+
+# Nautilus open any terminal extension
+curl -Lo /etc/yum.repos.d/nautilus-open-any-terminal.repo \
+  https://copr.fedorainfracloud.org/coprs/monkeygold/nautilus-open-any-terminal/repo/fedora-$(rpm -E %fedora)/monkeygold-nautilus-open-any-terminal-fedora-$(rpm -E %fedora).repo
+dnf install -y nautilus-open-any-terminal
+glib-compile-schemas /usr/share/glib-2.0/schemas
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal kitty
+
+# Installing Terminal Font
+mkdir -p /usr/share/fonts/maple-mono-nf
+curl -L -o /tmp/MapleMono-NF.zip https://github.com/subframe7536/maple-font/releases/download/v7.9/MapleMono-NF.zip
+dnf install -y unzip
+unzip -o /tmp/MapleMono-NF.zip -d /usr/share/fonts/maple-mono-nf/
+rm /tmp/MapleMono-NF.zip
+fc-cache -f -v
+
+# Copy Kitty Config
+mkdir -p /etc/skel/.config/kitty
+cp -rf /ctx/dot_config/kitty/kitty.conf /etc/skel/.config/kitty/
 
 systemctl enable podman.socket
+
+## 5. ZSH & Starship Configuration 
+sed -i 's|SHELL=/bin/bash|SHELL=/bin/zsh|' /etc/default/useradd
+
+mkdir -p /etc/zsh
+cat << 'EOF' > /etc/zsh/zshrc
+# Inizializzazione globale di Zsh
+if [ -x "$(command -v starship)" ]; then
+    eval "$(starship init zsh)"
+fi
+
+# Mantieni il completamento e la storia base
+autoload -U compinit && compinit
+HISTFILE=~/.zsh_history
+HISTSIZE=1000
+SAVEHIST=1000
+setopt appendhistory
+
+# Importa gli alias globali se esistono ancora
+if [ -f /etc/profile.d/origami-aliases.sh ]; then
+    source /etc/profile.d/origami-aliases.sh
+fi
+EOF
+
+# Install Spinner Theme
+dnf install -y plymouth-theme-spinner
+plymouth-set-default-theme spinner
+true
+
+# Disable Origami tips
+sudo mv /etc/profile.d/origami-aliases.sh /etc/profile.d/origami-aliases.sh.bak
+
+## CLEAN UP
+# Clean up dnf cache to reduce image size
+dnf5 -y clean all
+rm -rf /run/dnf /run/selinux-policy
+rm -rf /var/lib/dnf
